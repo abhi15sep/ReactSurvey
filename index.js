a4d7341a61c9a6f5def1d1ab2b6e356999e7612d
Ahnming Loke <lokeahnming@gmail.com>
2018-12-29 17:06:07 -0500

    feat: initial oauth login

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 
 const express = require('express');
 const passport = require('passport');
+
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('./config/keys');
-
 const app = express();
 
 /* testing authentication flow */
@@ -14,19 +14,32 @@ passport.use(
 	    clientSecret: keys.googleClientSecret,
 	    callbackURL: '/auth/google/callback'
     },
-    accessToken => {
-	    console.log(accessToken);
+    (accessToken, refreshToken, profile, done) => {
+	    console.log('accessToken: ', accessToken);
+	    console.log('refreshToken: ', refreshToken);
+	    console.log('profile: ', profile);
 	  }
 	)
 );
 
-/* single route handler, testing an ask from google to retrieve specific pieces of google account */
+/* 
+  single route handler,
+  testing an ask from google to retrieve specific pieces of google account 
+*/
 app.get(
   '/auth/google',
   passport.authenticate('google', {
     scope: ['profile', 'email']
   })
-)
+);
+
+/* testing second route handler for /auth/google/callback */
+app.get(
+	'/auth/google/callback',
+	passport.authenticate('google')
+);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+app.listen(PORT, () => {
+	console.log('listening');
+});
