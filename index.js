f48bfd0f5e189712a1c78cca09fe1970b413eb8d
Ahnming Loke <lokeahnming@gmail.com>
2018-12-29 17:31:54 -0500

    feat: added nodemon + startup script for dynamic updating, refactor for routing and services

--- index.js
+++ index.js
@@ -1,45 +1,11 @@
-
 const express = require('express');
-const passport = require('passport');
+require('./services/passport');
 
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys');
 const app = express();
 
-/* testing authentication flow */
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientID,
-	    clientSecret: keys.googleClientSecret,
-	    callbackURL: '/auth/google/callback'
-    },
-    (accessToken, refreshToken, profile, done) => {
-	    console.log('accessToken: ', accessToken);
-	    console.log('refreshToken: ', refreshToken);
-	    console.log('profile: ', profile);
-	  }
-	)
-);
-
-/* 
-  single route handler,
-  testing an ask from google to retrieve specific pieces of google account 
-*/
-app.get(
-  '/auth/google',
-  passport.authenticate('google', {
-    scope: ['profile', 'email']
-  })
-);
-
-/* testing second route handler for /auth/google/callback */
-app.get(
-	'/auth/google/callback',
-	passport.authenticate('google')
-);
-
+require('./routes/authRoutes')(app);
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
 	console.log('listening');
 });
