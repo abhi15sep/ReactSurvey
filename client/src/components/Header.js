6b5d66a6ea0db29dbb1425f44d9f5f523121b6b7
Ahnming Loke <lokeahnming@gmail.com>
2019-01-12 00:46:27 -0500

    feat: testing fetchUser action using axios

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,9 +4,9 @@ class Header extends Component {
 	render() {
 		return(
 			<nav>
-				<div class="nav-wrapper">
-				  <a href="#" class="left brand-logo">Emaily</a>
-				  <ul id="nav-mobile" class="right hide-on-med-and-down">
+				<div className="nav-wrapper">
+				  <a href="#" className="left brand-logo">Emaily</a>
+				  <ul id="nav-mobile" className="right hide-on-med-and-down">
 				    <li><a href="/">Login with Google</a></li>
 				  </ul>
 				</div>
--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,4 +1,5 @@
 export default function(state = {}, action) {
+	console.log('testing action: ', action);
 	switch(action.type) {
 		default: return state;
 	}
