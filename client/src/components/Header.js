810dc54f66be81bb3fc951b48825891f2c0ddf13
Ahnming Loke <lokeahnming@gmail.com>
2019-01-12 00:15:08 -0500

    feat: implement materialize.css for prototyping

---
+++ client/src/components/Header.js
@@ -0,0 +1,18 @@
+import  React, {Component } from 'react';
+
+class Header extends Component {
+	render() {
+		return(
+			<nav>
+				<div class="nav-wrapper">
+				  <a href="#" class="left brand-logo">Emaily</a>
+				  <ul id="nav-mobile" class="right hide-on-med-and-down">
+				    <li><a href="/">Login with Google</a></li>
+				  </ul>
+				</div>
+			 </nav>
+		);
+	}
+}
+
+export default Header;
