98f548bcadeaf18055a9125e38035d9c1e1c6dbc
Ahnming Loke <lokeahnming@gmail.com>
2019-01-18 07:44:02 -0500

    feat: add credits to user, require basic authentication

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -2,7 +2,13 @@ const keys = require('../config/keys');
 const stripe = require('stripe')(keys.stripeSecretKey);
 
 module.exports = app => {
+
   app.post('./api/stripe', async (req, res) => {
+
+		if (!req.user) {
+			return res.status(401).send({ error: "I am error. You're not logged in."});
+		}
+
   	const charge = await stripe.charges.create({
   		amount: 500,
   		currency: 'usd',
@@ -10,6 +16,8 @@ module.exports = app => {
   		source: req.body.id
   	});
 
-  	console.log(charge);
+		req.user.credts += 5;
+		const user = await req.user.save();
+		res.send(user);
   });
 };
